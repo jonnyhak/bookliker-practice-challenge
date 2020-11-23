704d1c544f01423087a8a8dfee38f06bddffe92c
jonnyhak <jonnyhak@gmail.com>
2020-11-23 11:51:03 -0500

    rendered books

--- js/index.js
+++ js/index.js
@@ -1,1 +1,32 @@
-document.addEventListener("DOMContentLoaded", function() {});
+document.addEventListener("DOMContentLoaded", function() {
+
+    const booksCollection = document.querySelector("#list")
+
+    const renderBook = (bookObj) => {
+        // booksCollection.dataset.id = ramenObj.id
+        const li = document.createElement('li')
+        li.textContent = bookObj.title
+
+        booksCollection.append(li)
+    }
+    
+    const getBooks = () => {
+        fetch('http://localhost:3000/books')
+            .then(response => {
+                return response.json()
+            }).then((booksArr) => {
+                booksArr.forEach((bookObj) => {
+                    renderBook(bookObj)
+                })
+            })
+    }
+
+   
+
+
+
+
+    getBooks()
+
+
+});
